b38ab79abb1bc97c53509885923a12298f5d34de
Lizaveta Rabtsava <lizaryabtsevav2@gmail.com>
2022-04-03 15:59:25 +0300

    changed message for NotFoundError class

--- dist/errors/NotFoundError.js
+++ dist/errors/NotFoundError.js
@@ -6,8 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const HttpError_1 = __importDefault(require("./HttpError"));
 class NotFoundError extends HttpError_1.default {
     constructor(id) {
-        if (id) {
+        if (typeof id === 'number') {
             super(`Record with id = ${id} not found!`, 404);
+        }
+        else if (typeof id === 'string') {
+            super(id, 404);
         }
         else {
             super(`Records was not found!`, 404);
--- src/errors/NotFoundError.ts
+++ src/errors/NotFoundError.ts
@@ -1,9 +1,11 @@
 import HttpError from './HttpError';
 
 class NotFoundError extends HttpError {
-    constructor(id?: string) {
-        if (id) {
+    constructor(id?: number | string) {
+        if (typeof id === 'number') {
             super(`Record with id = ${id} not found!`, 404);
+        } else if (typeof id === 'string') {
+            super(id, 404);
         } else {
             super(`Records was not found!`, 404);
         }
